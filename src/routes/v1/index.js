2c9e3f11900a4b4ebb60aa889af88670e7172d1c
Faraz Mobeen <faraz.mobin17@gmail.com>
2024-05-02 18:03:03 +0530

    Revert "Feature setup mvc problem service"

--- src/routes/v1/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const ProblemRouter = require("./problem.routes");
-const v1Router = express.Router();
-
-v1Router.use("/v1", ProblemRouter);
-
-module.exports = v1Router;
