e682387b652a4ef5319530c677c0026e32032ad6
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-05-02 17:58:45 +0530

    migrated from status code to status code library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,21 +1,32 @@
+const { StatusCodes } = require("http-status-codes");
 function pingProblemController(req, res) {
-  return res.status(200).json({ message: "Ping Controller is Alive" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Ping Controller is Alive" });
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({ message: "not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "not implemented" });
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({ message: "not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "not implemented" });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({ message: "not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "not implemented" });
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({ message: "not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "not implemented" });
 }
 
 function updateProblem(req, res) {
